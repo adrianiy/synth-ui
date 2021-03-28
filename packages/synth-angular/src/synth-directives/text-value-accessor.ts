dece1399374149093ec557288d83762e805d26b6
adrianiy <adrianiy@ext.inditex.com>
2021-03-28 20:04:50 +0200

    fix(docs): fix docs

--- packages/synth-angular/src/synth-directives/text-value-accessor.ts
+++ packages/synth-angular/src/synth-directives/text-value-accessor.ts
@@ -4,21 +4,21 @@ import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ValueAccessor } from './value-accessor';
 
 @Directive({
-  /* tslint:disable-next-line:directive-selector */
-  selector: 'my-input[type=text]',
-  host: {
-    '(myChange)': 'handleChangeEvent($event.target.value)'
-  },
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: TextValueAccessor,
-      multi: true
-    }
-  ]
+    /* tslint:disable-next-line:directive-selector */
+    selector: 'my-input[type=text]',
+    host: {
+        '(myChange)': 'handleChangeEvent($event.target.value)'
+    },
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: TextValueAccessor,
+            multi: true
+        }
+    ]
 })
 export class TextValueAccessor extends ValueAccessor {
-  constructor(el: ElementRef) {
-    super(el);
-  }
+    constructor(el: ElementRef) {
+        super(el);
+    }
 }
