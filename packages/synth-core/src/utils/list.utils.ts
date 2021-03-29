726bbe7bf7dc9efcc2ac04b78efe8609b89ea4c8
adrianiy <adrianiy@ext.inditex.com>
2021-03-29 02:42:39 +0200

    fix(angular): use shared list utils from synth-core

--- packages/synth-core/src/utils/list.utils.ts
+++ packages/synth-core/src/utils/list.utils.ts
@@ -6,18 +6,19 @@ export const configRow = (row: Row, fieldsConfig: FieldsConfig[]) => {
 
     return {
         name: capitalize(row.name),
-        children: _parseChildren(row.children, fieldsConfig),
-        ...fields
+        _isTotal: row._isTotal,
+        _children: _parseChildren(row.children, fieldsConfig),
+        ...fields,
     };
 };
 
 const _configFields = (row: Row, fieldsConfig: FieldsConfig[]) => {
-    return fieldsConfig.map(field => [ field.title(), _getValue(row, field) ]);
+    return fieldsConfig.map((field) => [ field.title(), _getValue(row, field) ]);
 };
 
 const _parseChildren = (children: Row[], fieldsConfig: FieldsConfig[]) => {
     if (children) {
-        return children.map(row => _configFields(row, fieldsConfig));
+        return children.map((row) => _configFields(row, fieldsConfig));
     } else {
         return Array(3)
             .fill(0)
@@ -26,11 +27,14 @@ const _parseChildren = (children: Row[], fieldsConfig: FieldsConfig[]) => {
 };
 
 const _getValue = (row: any, fieldConfig: FieldsConfig) => {
-    const { field, format, decoration } = fieldConfig;
+    const { value, format, decoration, sign, negativeSign, showZero } = fieldConfig;
 
     return {
-        value: row[field()],
-        format: format(),
-        decoration: decoration()
+        value: row[value()],
+        decoration: decoration?.(),
+        foramt: format?.(),
+        sign: sign?.(),
+        negativeSign: negativeSign?.(),
+        showZero: showZero?.(),
     };
 };
