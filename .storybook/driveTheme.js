d706ae691a27e7aa47e0741161276e6b27dfeb42
adrianiy <adrianiy@ext.inditex.com>
2021-04-16 01:01:08 +0200

    chore: update mdx

--- .storybook/driveTheme.js
+++ .storybook/driveTheme.js
@@ -3,5 +3,5 @@ import { create } from '@storybook/theming';
 export default create({
     base: 'light',
     brandTitle: 'SynthUI',
-    brandImage: 'https://placehold.it/350x150',
+    brandImage: './logo.png',
 });
