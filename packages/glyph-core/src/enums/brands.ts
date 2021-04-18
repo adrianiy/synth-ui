b6e0db555d87b350ce2308d4db2088d9d85fa1ab
adrianiy <adrianiy@ext.inditex.com>
2021-04-18 19:31:17 +0200

    feat(core): add brand and event enums

---
+++ packages/glyph-core/src/enums/brands.ts
@@ -0,0 +1,12 @@
+export const enum Brands {
+    zara = 'zara',
+    pull = 'pull&bear',
+    bershka = 'bershka',
+    stradivarius = 'stradivarius',
+    oysho = 'oysho',
+    lefties = 'lefties',
+    massimo = 'massimoDutti',
+    home = 'zaraHome',
+    south = 'zaraSouth',
+    uterque = 'uterque',
+}
