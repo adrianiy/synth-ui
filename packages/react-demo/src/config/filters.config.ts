a73991b02bacb5629a8ddfdd1bce085a9a43cbf2
adrianiy <adrianiy@ext.inditex.com>
2021-04-26 00:29:48 +0200

    fix(react): fix react playground filters

--- packages/react-demo/src/config/filters.config.ts
+++ packages/react-demo/src/config/filters.config.ts
@@ -1,69 +1,30 @@
 export const FiltersConfig = {
-    search: {
-        usableIn: [ 'test' ],
-        key: 'search',
-        selected: [],
-        lastSearchs: [],
-        activePos: -1,
-        suggestions: [],
-        commonSearchs: [
-            [
-                { description: 'Ropa', type: 'product', parent: 'Producto', code: 0 },
-                { description: 'señora', type: 'product', parent: 'Sección', code: 1 }
-            ],
-            [
-                {
-                    description: 'Calzado',
-                    type: 'product',
-                    parent: 'Producto',
-                    code: 1
-                },
-                {
-                    description: 'caballero',
-                    type: 'product',
-                    parent: 'Sección',
-                    code: 2
-                },
-                { description: 'alovera', type: 'location', parent: 'Mercado', code: 1 }
-            ]
-        ],
-        filters: []
-    },
-    testDate: {
-        usableIn: [ 'test' ],
-        description: 'Mes',
-        plural: 'Meses',
-        key: 'test_date',
-        visible: true,
-        selected: [],
-        options: []
-    },
     product: {
-        usableIn: [ 'test' ],
+        usableIn: ['test'],
         description: 'Producto',
         plural: 'Productos',
         key: 'cod_product',
         visible: true,
         selected: [],
-        options: []
+        options: [],
     },
     section: {
-        usableIn: [ 'test' ],
+        usableIn: ['test'],
         description: 'Sección',
         plural: 'Secciones',
         key: 'cod_section',
         visible: true,
         selected: [],
-        options: []
+        options: [],
     },
     market: {
-        usableIn: [ 'test' ],
+        usableIn: ['test'],
         description: 'Mercado',
         plural: 'Mercados',
         key: 'cod_market',
         searchPlaceholder: 'Busca Alovera, Corea ...',
         visible: true,
         selected: [],
-        options: []
-    }
+        options: [],
+    },
 };
