42c2f63bde2400e62f35eed420a196b9b140ead3
adrianiy <adrianiy@ext.inditex.com>
2021-07-01 01:20:57 +0200

    docs: fix drive theme in all packages

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,1 +1,7 @@
+import { addons } from '@storybook/addons';
+import driveTheme from './driveTheme';
 import '!style-loader!css-loader!sass-loader!./re-style-manager.scss';
+
+addons.setConfig({
+    theme: driveTheme,
+});
--- packages/glyph-react/.storybook/manager.js
+++ packages/glyph-react/.storybook/manager.js
@@ -1,1 +1,7 @@
+import { addons } from '@storybook/addons';
+import driveTheme from 'common-storybook/driveTheme';
 import '!style-loader!css-loader!sass-loader!common-storybook/re-style-manager.scss';
+
+addons.setConfig({
+    theme: driveThemen,
+});
