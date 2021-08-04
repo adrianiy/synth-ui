5643d18d9fb7fdce5ca294fa0988dcf3b00ca9b4
Adrian Insua <adrianiy@ext.inditex.com>
2021-08-04 23:56:56 +0200

    feat(core): export join and group utils

--- packages/glyph-core/src/utils/index.ts
+++ packages/glyph-core/src/utils/index.ts
@@ -2,4 +2,6 @@ export { configRow } from './list.utils';
 export { errorHandler } from './error.utils';
 export { sortCountries } from './sort.utils';
 export { getEntityName, getGrowth } from './data.utils';
+export { groupData } from './group.utils';
+export { crossJoin, leftOuterJoin } from './join.utils';
 export * as logger from './log.utils';
