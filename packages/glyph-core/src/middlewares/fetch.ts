e46c5e7cdb90d52a38788c66ef42d148570cff89
Adrian Insua <adrianiy@ext.inditex.com>
2021-08-05 00:50:19 +0200

    fix(core): get parameters from ctx instead of query

--- packages/glyph-core/src/middlewares/fetch.ts
+++ packages/glyph-core/src/middlewares/fetch.ts
@@ -33,8 +33,7 @@ export const fetchData = ({
         const queryParams = {};
 
         Object.keys(parameters).forEach((key: string) => {
-            const parameter =
-                getFrom(ctx.state, parameters[key]) || getFrom(ctx.query, parameters[key]) || parameters[key];
+            const parameter = getFrom(ctx.state, parameters[key]) || getFrom(ctx, parameters[key]) || parameters[key];
             queryParams[key] = is(parameter, Function) ? parameter(ctx) : parameter;
         });
 
