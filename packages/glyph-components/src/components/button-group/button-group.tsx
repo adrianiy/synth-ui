529c4d6c9db7cd746e1d0e147385c354d31cf74e
Adrian Insua <adrianiy@ext.inditex.com>
2021-08-08 00:30:47 +0200

    feat(button-group): add redesign styles in button group

--- packages/glyph-components/src/components/button-group/button-group.tsx
+++ packages/glyph-components/src/components/button-group/button-group.tsx
@@ -1,5 +1,5 @@
 import { Component, Prop, h } from '@stencil/core';
-import { Button, ButtonGroupStyle, Alignment } from 'glyph-core';
+import { Button, ButtonGroupStyle, Alignment, UIInterface } from 'glyph-core';
 import { Format } from '../../utils/format';
 import { Flex } from '../../utils/layout';
 import { cls } from '../../utils/utils';
@@ -16,6 +16,8 @@ export class ButtonGroupComponent {
     @Prop() size: ButtonGroupStyle = ButtonGroupStyle.big;
     /** Button alignment ['left', 'center', 'right'] */
     @Prop() alignment: Alignment = Alignment.center;
+    /** Application interface */
+    @Prop() interface: UIInterface = UIInterface.classic;
 
     private _handleClick = (button: Button, idx: number) => () => {
         this.buttons = this.buttons.map((btn, index) => ({ ...btn, active: index === idx }));
@@ -24,23 +26,39 @@ export class ButtonGroupComponent {
 
     private _renderTitle = (button: Button) => {
         return (
-            <h3>
+            <h3
+                class={cls({
+                    'number': button.title.isNumber,
+                    'label-l': true,
+                    'label--l--medium': !button.title.isNumber && this.interface === UIInterface.redesign,
+                })}
+            >
                 <Format config={button.title} decorate />
             </h3>
         );
     };
 
     private _renderSubtitle = (button: Button) => {
-        return (
+        return this.interface !== UIInterface.redesign ? (
             <span class="caption caption--small">
                 <Format config={button.subtitle} decorate />
             </span>
+        ) : (
+            <h3
+                class={cls({
+                    'number': button.subtitle?.isNumber,
+                    'label-l': true,
+                    'label--l--medium': !button.subtitle.isNumber && this.interface === UIInterface.redesign,
+                })}
+            >
+                <Format config={button.subtitle} decorate />
+            </h3>
         );
     };
 
     render() {
         return (
-            <Flex row class={cls('button-group__container', this.size)}>
+            <Flex row class={cls('button-group__container', this.interface, this.size)}>
                 {this.buttons.map((button, idx) => (
                     <div
                         class={cls('button', { active: button.active }, this.alignment)}
