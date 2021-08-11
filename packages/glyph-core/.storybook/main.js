83b32abfb9e5156f6bcdbe729548e924127e72e3
Adrian Insua <adrianiy@ext.inditex.com>
2021-08-11 20:39:30 +0200

    fix(doc): fix core doc build

--- packages/glyph-core/.storybook/main.js
+++ packages/glyph-core/.storybook/main.js
@@ -1,4 +1,15 @@
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
 module.exports = {
+    webpackFinal: async (config, { configType }) => {
+        // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+        // You can change the configuration based on that.
+        // 'PRODUCTION' is used when building the static version of storybook.
+
+        config.plugins.push(new ProgressBarPlugin());
+        // Return the altered config
+        return config;
+    },
     stories: [ '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)' ],
     addons: [ '@storybook/addon-links', '@storybook/addon-essentials', 'creevey' ],
 };
