c00497f2a9b401235cf7a0e45916e4573eebdff3
Adrian Insua <adrianiy@ext.inditex.com>
2021-09-10 12:38:37 +0200

    fix: fix filters middleware typping

--- packages/glyph-core/src/models/middlewares.ts
+++ packages/glyph-core/src/models/middlewares.ts
@@ -1,12 +1,12 @@
 export interface FiltersConfigMiddleware {
     endpoints?: {
-        sections?: string;
-        products?: string;
-        product_lines?: string;
-        campaigns?: string;
-        markets?: string;
-        platforms?: string;
-        warehouses?: string;
+        sections?: any;
+        products?: any;
+        product_lines?: any;
+        campaigns?: any;
+        markets?: any;
+        platforms?: any;
+        warehouses?: any;
     };
     baseUrl?: string;
     auth?: string;
